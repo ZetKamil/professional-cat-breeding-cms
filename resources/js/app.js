5a06923ae5b810ede72fcc581c3254d011f7f26a
ZetKamil <kamil.ziemkiewicz.97@gmail.com>
2026-08-11 21:36:33 +0200

    fix(backend): include Bootstrap 5 JS bundle script in shell layout to enable topbar dropdown menu

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,3 @@
-// Bootstrap setup (Laravel default)
-//import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-// resources/js/app.js
-
-// Laadt Bootstrap JS (incl. Alert plugin + data-bs-dismiss support)
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-// // Alpine
-// import Alpine from 'alpinejs'
-// window.Alpine = Alpine
-// Alpine.start()
-
 // SB Admin core script
 import './sb-admin/scripts'
 
